8ff3ea01365fb016a300c71d03cb76df3f715c53
jonasmachados <jonasmachado.ti@gmail.com>
2021-02-21 22:14:03 -0300

    Implementing the Interface Mecanico Repository

---
+++ src/main/java/com/Jonas/AppWebOficina/MecanicoRepository.java
@@ -0,0 +1,17 @@
+
+package com.Jonas.AppWebOficina;
+
+import com.Jonas.AppWebOficina.domain.Mecanico;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author Jonas, created 21/02/2021
+ */
+@Repository
+public interface MecanicoRepository extends JpaRepository<Mecanico, Integer>{
+
+    
+
+}
