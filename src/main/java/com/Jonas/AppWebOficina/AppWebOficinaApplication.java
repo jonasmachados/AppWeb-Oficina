506e59f89cf301dbb2e4541b2c6d318e41e44e58
jonasmachados <jonasmachado.ti@gmail.com>
2021-02-21 21:45:52 -0300

    Project created

---
+++ src/main/java/com/Jonas/AppWebOficina/AppWebOficinaApplication.java
@@ -0,0 +1,13 @@
+package com.Jonas.AppWebOficina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppWebOficinaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppWebOficinaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Jonas/AppWebOficina/AppWebOficinaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Jonas.AppWebOficina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppWebOficinaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
