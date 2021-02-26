f9da44afbb12b4d45379424e31af96db9ec06562
jonasmachados <jonasmachado.ti@gmail.com>
2021-02-25 21:52:12 -0300

    Add more objects

--- src/main/java/com/Jonas/AppWebOficina/AppWebOficinaApplication.java
+++ src/main/java/com/Jonas/AppWebOficina/AppWebOficinaApplication.java
@@ -50,19 +50,24 @@ public class AppWebOficinaApplication implements CommandLineRunner {
         Carro c1 = new Carro(null, "Celta", "Chevrolet", 2004, "Renavan 11", "Chassi 22", "Prata", "Gasolina");
         
         Mecanico m1 = new Mecanico(null, "Grand Pneus", 111, "37520-000", "Avenida Joao Pinheiros", 1008, "Centro", "Poços de Caldas", 36631724, "Observacoes", sdf.parse("21/02/2021"));
-        
+        Mecanico m2 = new Mecanico(null, "CASA PRADO", 111, "37520-000", "MARECHAL DEODORO", 589, "Centro", "Poços de Caldas", 37145878, "Observacoes", sdf.parse("25/02/2021"));
         Pecas p1 = new Pecas(null, "Retrovisor", sdf.parse("21/02/2021"), 100.0);
         Pecas p2 = new Pecas(null, "Parachoque", sdf.parse("21/02/2021"), 250.0);
         
-        Servico s1 = new Servico(null, "Troca retrovisor", 120.0 , sdf.parse("23/02/2021"), 90000.0, m1, c1);
+        Pecas p3 = new Pecas(null, "FUZIVEL", sdf.parse("25/02/2021"), 40.0);
+        Pecas p4 = new Pecas(null, "BATERIA", sdf.parse("25/02/2021"), 125.0);
         
+        Servico s1 = new Servico(null, "Troca retrovisor", 120.0 , sdf.parse("23/02/2021"), 90000.0, m1, c1);
+        Servico s2 = new Servico(null, "TROCA FUZIL", 40.0 , sdf.parse("25/02/2021"), 90005.0, m2, c1);
+        Servico s3 = new Servico(null, "TROCA BATERIA", 125.0 , sdf.parse("23/02/2021"), 90005.0, m2, c1);
+
         s1.getPecas().addAll(Arrays.asList(p1, p2));
         
         
         this.carroRepository.saveAll(Arrays.asList(c1));
-        this.mecanicoRepository.saveAll(Arrays.asList(m1));
-        this.pecaRepository.saveAll(Arrays.asList(p1,p2));
-        this.servicoRepository.saveAll(Arrays.asList(s1));
+        this.mecanicoRepository.saveAll(Arrays.asList(m1, m2));
+        this.pecaRepository.saveAll(Arrays.asList(p1,p2, p3, p4));
+        this.servicoRepository.saveAll(Arrays.asList(s1, s2, s3));
         
     }
 
