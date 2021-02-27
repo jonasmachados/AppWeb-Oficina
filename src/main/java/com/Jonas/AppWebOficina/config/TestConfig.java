e767de1598f3e1013f8cb2792f98a0379444905c
jonasmachados <jonasmachado.ti@gmail.com>
2021-02-27 11:18:29 -0300

    Creating profile test

---
+++ src/main/java/com/Jonas/AppWebOficina/config/TestConfig.java
@@ -0,0 +1,27 @@
+package com.Jonas.AppWebOficina.config;
+
+import com.Jonas.AppWebOficina.service.DBService;
+import java.text.ParseException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+/**
+ *
+ * @author Jonas, created 27/02/2021
+ */
+@Configuration
+@Profile("test")
+public class TestConfig {
+
+    @Autowired
+    private DBService dbService;
+
+    //Method that will go make dependency injection
+    @Bean
+    public void instanciaBaseDeDados() throws ParseException {
+        this.dbService.instanciaBaseDeDados();
+    }
+
+}
