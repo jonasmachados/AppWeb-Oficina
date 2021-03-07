62209d5892647f86c9a3eb39e1a14d411ec31a6a
jonasmachados <jonasmachado.ti@gmail.com>
2021-03-07 10:31:28 -0300

    Put and Path to a car

--- src/main/java/com/Jonas/AppWebOficina/resource/CarroResource.java
+++ src/main/java/com/Jonas/AppWebOficina/resource/CarroResource.java
@@ -11,7 +11,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.ResponseEntity;
-
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 /**
  *
@@ -20,22 +22,33 @@ import org.springframework.http.ResponseEntity;
 @RestController
 @RequestMapping(value = "/carros")
 public class CarroResource {
-    
+
     @Autowired
     private CarroService service;
-    
+
     @GetMapping(value = "/{id}")
-    public ResponseEntity<Carro> findById(@PathVariable Integer id){
+    public ResponseEntity<Carro> findById(@PathVariable Integer id) {
         Carro obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
-        
     }
-    
+
     @GetMapping
-    public ResponseEntity<List<CarroDTO>> findALl(){
+    public ResponseEntity<List<CarroDTO>> findALl() {
         List<Carro> list = service.findAll();
         List<CarroDTO> listDTO = list.stream().map(obj -> new CarroDTO(obj)).collect(Collectors.toList());
         return ResponseEntity.ok().body(listDTO);
     }
-    
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<Carro> update(@PathVariable Integer id, @RequestBody Carro obj) {
+        Carro newObj = service.update(id, obj);
+        return ResponseEntity.ok().body(newObj);
+    }
+
+    @PatchMapping(value = "/{id}")
+    public ResponseEntity<Carro> updatePatch(@PathVariable Integer id, @RequestBody Carro obj) {
+        Carro newObj = service.update(id, obj);
+        return ResponseEntity.ok().body(newObj);
+    }
+
 }
--- src/main/java/com/Jonas/AppWebOficina/service/CarroService.java
+++ src/main/java/com/Jonas/AppWebOficina/service/CarroService.java
@@ -23,11 +23,26 @@ public class CarroService {
         Optional<Carro> obj = repository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException(
                 "Objeto não encontrado! Id: " + id + ", Tipo: " + Servico.class.getName()));
-
     }
 
     public List<Carro> findAll() {
         return repository.findAll();
+    }
+
+    public Carro update(Integer id, Carro obj) {
+        Carro newObj = findById(id);
+        updateData(newObj, obj);
+        return repository.save(newObj);
+    }
+
+    private void updateData(Carro newObj, Carro obj) {
+        newObj.setModelo(obj.getModelo());
+        newObj.setMarca(obj.getMarca());
+        newObj.setAno(obj.getAno());
+        newObj.setChassi(obj.getChassi());
+        newObj.setRenavan(obj.getRenavan());
+        newObj.setCor(obj.getCor());
+        newObj.setCombustivel(obj.getCombustivel());
     }
 
 }
