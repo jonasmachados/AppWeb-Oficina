d1ba46bf1a292202882aec904e1a8237c9439933
jonasmachados <jonasmachado.ti@gmail.com>
2021-03-18 20:35:29 -0300

    Fix DataIntegrityViolationException, to delete a mechanic with associated

--- src/main/java/com/Jonas/AppWebOficina/service/MecanicoService.java
+++ src/main/java/com/Jonas/AppWebOficina/service/MecanicoService.java
@@ -1,14 +1,13 @@
 package com.Jonas.AppWebOficina.service;
 
-import com.Jonas.AppWebOficina.domain.Carro;
 import com.Jonas.AppWebOficina.domain.Mecanico;
 import com.Jonas.AppWebOficina.repositories.MecanicoRepository;
-import com.Jonas.AppWebOficina.service.exceptions.DataIntegrityViolationException;
 import com.Jonas.AppWebOficina.service.exceptions.ObjectNotFoundException;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.Optional;
+import org.springframework.dao.DataIntegrityViolationException;
 
 /**
  *
